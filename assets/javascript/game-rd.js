1747ccb8d44cde3eaefe6c3edd676a0d8dec8df9
Rex <roydavis99.rd@gmail.com>
2018-08-21 21:25:24 -0700

    insert game-rd.js

---
+++ assets/javascript/game-rd.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    $("#th-play").on("click", function(){
+        alert("I am playing");
+    });
+
+    $("#th-stop").on("click", function(){
+        alert("I am stop")
+    });
+});
